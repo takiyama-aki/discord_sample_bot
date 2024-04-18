65f209f5d495fa224b21e9124a564b3210d5056a
akki <takiyamaakihito@gmail.com>
2024-04-18 23:33:10 +0900

    おみくじ機能の追加

--- sample_bot.py
+++ sample_bot.py
@@ -1,13 +1,23 @@
 import discord
 from discord.ext import commands
+import random
 
 
 client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
+OMIKUJI = ['大吉', '吉', '中吉', '小吉', '末吉', '凶', '大凶']
 
 @client.event
 async def on_ready():
     print('ログインしました')
+
+@client.command()
+async def hello(ctx):
+    await ctx.send('Hello!')
+
+@client.command()
+async def omikuji(ctx):
+    await ctx.send(f'あなたの運勢は"{OMIKUJI[random.randrange(len(OMIKUJI))]}"!')
 
 @client.command()
 async def test(ctx):
@@ -16,6 +26,8 @@ async def test(ctx):
 @client.command()
 async def hoge(ctx):
     await ctx.send('ほげほげ')
+
+
 
 TOKEN = 'ここにトークン'
 client.run(TOKEN)
